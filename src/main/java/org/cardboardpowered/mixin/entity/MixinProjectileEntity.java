73eb568ddcc06d62099f169718f90a6950601efb
Isaiah <16439221+pisaiah@users.noreply.github.com>
2025-09-21 23:12:46 -0400

    1.21.6: Fix MixinProjectileEntity

--- src/main/java/org/cardboardpowered/mixin/entity/MixinProjectileEntity.java
+++ src/main/java/org/cardboardpowered/mixin/entity/MixinProjectileEntity.java
@@ -6,11 +6,12 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
+import org.bukkit.craftbukkit.entity.CraftEntity;
 import org.bukkit.craftbukkit.event.CraftEventFactory;
 import org.cardboardpowered.interfaces.IMixinEntity;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LazyEntityReference;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
@@ -19,8 +20,8 @@ import net.minecraft.util.hit.HitResult;
 public class MixinProjectileEntity extends MixinEntity {
 
     @Inject(at = @At("TAIL"), method = "setOwner")
-    public void setProjectileSource(Entity entity, CallbackInfo ci) {
-        this.projectileSource = (entity != null && ((IMixinEntity)entity).getBukkitEntity() instanceof ProjectileSource) ? (ProjectileSource) ((IMixinEntity)entity).getBukkitEntity() : null;
+    public void setProjectileSource(LazyEntityReference<Entity> entity, CallbackInfo ci) {
+    	cb$refreshProjectileSource(false);
     }
 
     @Inject(at = @At("HEAD"), method = "onCollision")
@@ -30,6 +31,23 @@ public class MixinProjectileEntity extends MixinEntity {
 
     @Shadow
     public void onBlockHit(BlockHitResult blockHitResult) {
+    }
+    
+    private void cb$refreshProjectileSource(boolean fillCache) {
+        CraftEntity craftEntity;
+        Entity owner;
+        if (fillCache) {
+            this.getOwner();
+        }
+        if ((owner = this.getOwner()) != null && this.projectileSource == null && (craftEntity = ((IMixinEntity)owner).getBukkitEntity()) instanceof ProjectileSource) {
+            ProjectileSource source = (ProjectileSource)craftEntity;
+            this.projectileSource = source;
+        }
+    }
+    
+    @Shadow
+    public Entity getOwner() {
+        return null; // Shadowed
     }
 
 }
